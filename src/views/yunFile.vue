420157a90c97b4f8217bcd03ccbc60dc631bc1db
huanghaojie <1107408003@qq.com>
2019-06-10 18:36:17 +0800

    移动端 分享给我的文档

---
+++ src/views/yunFile.vue
@@ -0,0 +1,193 @@
+<template>
+  <div ref="wrap">
+    <!-- 云盘 -->
+    <div class="yun-file">
+      <div class="pb35 mt20">
+        <div class="yun-file-title flex">
+          <text class="f28 fw5 c0">分享给我的文档</text>
+          <text class="f24 c153 fw4 pl20 pt10 pb10" @click="yunFileMoreEvent">全部</text>
+        </div>
+        <div class="mt36 prl20 file-height">
+          <div class="flex" v-if='isShowTM'>
+            <scroller v-if='yunFileToMeArr.length != 0' class="yun-scroller flex-dr" show-scrollbar='false'
+              scroll-direction="horizontal">
+              <div class="flex-ac file-name" v-for="(item, index) in yunFileToMeArr" :key='index'
+                @click='yunFileUserEvent(item.id)'>
+                <bui-image :src="item.image" width="52px" height="52px"></bui-image>
+                <div class="flex-jc flex-ac" style="width: 153px">
+                  <text class="f24 c51 fw4 mt17 lines2">{{item.name}}</text>
+                </div>
+              </div>
+            </scroller>
+            <div class="no-content flex-ac flex-jc" v-if='yunFileToMeArr.length==0'>
+              <div class="flex-dr flex-jc">
+                <bui-image src="/image/sleep1.png" width="42px" height="39px"></bui-image>
+                <text class="f26 c51 fw4 pl15 center-height ">{{isErrorTM?'暂无文档':'加载失败'}}</text>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  const link = weex.requireModule("LinkModule");
+  const linkapi = require("linkapi");
+  const dom = weex.requireModule('dom');
+  export default {
+    data() {
+      return {
+        yunFileToMeArr: [],
+        isErrorTM: true,
+        isShowTM: false,
+        channel: new BroadcastChannel('WidgetsMessage')
+      }
+    },
+    methods: {
+      yunFileMoreEvent() {
+        link.launchLinkService(['[OpenBuiltIn] \n key=MyDisk'], (res) => {}, (err) => {});
+      },
+      yunFileUserEvent(id) {
+        link.launchLinkService(['[OpenBuiltIn] \n key=DiskDetail \n diskId=' + id], (res) => {}, (err) => {});
+      },
+      getFileImages(ext) {
+        let fileImageTypes = {
+          'excel': ['xls', 'xlsx'],
+          'music': ['mp3', 'wma', 'wav', 'mod', 'ogg', 'm4a'],
+          'pdf': ['pdf'],
+          'photo': ['bmp', 'gif', 'jpeg', 'jpg', 'svg', 'png', 'psd'],
+          'ppt': ['ppt', 'pptx'],
+          'txt': ['txt', 'key'],
+          'video': ['rm', 'rmvb', 'wmv', 'avi', 'mp4', '3gp', 'mkv', 'flv', 'mov', 'mpg'],
+          'word': ['doc', 'docx', 'wps'],
+          'zip': ['zip', 'rar', '7z'],
+          'unknow': ['file'],
+          'folder2': ['folder'],
+          'team': ['team']
+        }
+        let fileImages = {};
+        let fileTypeImages = {};
+        for (let fext in fileImageTypes) {
+          fileImages[fext] = '/image/' + fext + '.png';
+          let arr = fileImageTypes[fext];
+          if (arr.length > 0) {
+            for (let i = 0; i < arr.length; i++) {
+              fileTypeImages[arr[i]] = fext;
+            }
+          }
+        }
+        let type = fileTypeImages[ext];
+        if (type) {
+          return fileImages[type];
+        } else {
+          return fileImages['unknow'];
+        }
+      },
+      getYunFile() {
+        link.getServerConfigs([], (params) => {
+          link.getLoginInfo([], (user) => {
+            let objDataTo = {
+              by: '',
+              to: 'U' + user.userId,
+              scope: ''
+            }
+            linkapi.get({
+              url: params.diskUri + 'openapi//file/share/list',
+              data: objDataTo
+            }).then((res) => {
+              this.isShowTM = true
+              this.isErrorTM = true
+              let fileArr = []
+              for (let index = 0, resLength = res.rows.length; index < resLength; index++) {
+                let fileObj = {}
+                const element = res.rows[index];
+                fileObj['name'] = element.name
+                fileObj['id'] = element.fileId
+                if (element.type == 'D') {
+                  fileObj['image'] = '/image/word.png'
+                } else {
+                  fileObj['image'] = this.getFileImages(element.extension)
+                }
+                fileArr.push(fileObj)
+              }
+              this.yunFileToMeArr = fileArr
+              this.broadcastWidgetHeight()
+            }, (err) => {
+              this.isShowTM = true
+              this.isErrorTM = false
+              this.broadcastWidgetHeight()
+            })
+          }, (err) => {
+            this.error()
+          })
+        }, () => {
+          this.error()
+        });
+      },
+      error() {
+        this.isShowTM = true
+        this.isErrorTM = false
+        this.broadcastWidgetHeight()
+      },
+      broadcastWidgetHeight() {
+        let _params = this.$getPageParams();
+        setTimeout(() => {
+          dom.getComponentRect(this.$refs.wrap, (ret) => {
+            this.channel.postMessage({
+              widgetHeight: ret.size.height,
+              id: _params.id
+            });
+            channel.close();
+          });
+        }, 100)
+      }
+    },
+    mounted() {
+      this.channel.onmessage = (event) => {
+        if (event.data.action === 'RefreshData') {
+          this.getYunFile()
+        }
+      }
+      this.getYunFile()
+    }
+  }
+</script>
+
+<style lang="css" src="../css/common.css"></style>
+<style>
+  .yun-file {
+    background-color: #fff;
+  }
+
+  .yun-file-title {
+    height: 40px;
+    margin: 18px 23px 36px 25px;
+  }
+
+
+  .file-name {
+    margin-right: 15px;
+    width: 166px;
+  }
+
+  .yun-scroller {
+    width: 720px;
+    height: 140px;
+  }
+
+  .no-file {
+    height: 166px;
+    width: 750px
+  }
+
+  .no-content {
+    height: 166px;
+    width: 750px
+  }
+
+  .center-height {
+    line-height: 40px;
+  }
+</style>
