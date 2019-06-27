314ab0979a8e2dbc692f7126e0aea402d6b906c1
huanghaojie <13612989029@163.com>
2019-06-27 13:43:02 +0800

    更换为更多, 高度通知多次

--- src/js/lang.js
+++ src/js/lang.js
@@ -1,19 +1,19 @@
 module.exports = {
     "zh-cn": {
         "FileSharedToMe": "分享给我的文档",
-        "All": "全部",
+        "All": "更多",
         "NoneData": "暂无文档",
         "ErrorLoadData": "加载失败"
     },
     "zh-hk": {
         "FileSharedToMe": "分享给我的文檔",
-        "All": "全部",
+        "All": "更多",
         "NoneData": "暫無檔案",
         "ErrorLoadData": "加載失敗"
     },
     "en": {
         "FileSharedToMe": "File shared to me",
-        "All": "All",
+        "All": "More",
         "NoneData": "No archives data",
         "ErrorLoadData": "loading failed"
     }
